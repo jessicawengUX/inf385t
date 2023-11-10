dcf9c68f6a53faae37e72b607824dba6ddfc8fd3
Jessica Weng <jessicaweng@wengcaixuansMBP.attlocal.net>
2023-11-09 21:26:11 -0600

    update navbar (top and sidebar). The app.js part may need to change or the component need to be update for responsive screen.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
+import './App.css'; // Import your global styles
+import "bootstrap/dist/css/bootstrap.css";
+
  // We import all the components we need in our app
 import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
 import Login from "./components/login";
 import Register from "./components/register";
 import Form from "./components/form";
 import Table from "./components/table";
+
  const App = () => {
  return (
    <div>
      <Navbar />
-     <Routes>
-        <Route exact path="/" element={<Form/>} />
-        <Route path="/app" element={<Form />} />
-        <Route path="/table" element={<Table />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-     </Routes>
+     <div className="d-flex flex-row"> 
+      <div className="flex-fill">
+          <Sidebar />
+      </div>
+      <div className="flex-fill">
+        <Routes>
+            <Route exact path="/" element={<Form/>} />
+            <Route path="/app" element={<Form />} />
+            <Route path="/table" element={<Table />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+        </div>
+     </div>
    </div>
  );
 };
