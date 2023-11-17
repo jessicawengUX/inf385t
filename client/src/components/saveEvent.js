6c7bada6d40388c7eb1b1df110efc60606b626d3
axj65 <axj65@my.utexas.edu>
2023-11-17 09:57:07 -0600

    saveEvent update test

--- client/src/components/saveEvent.js
+++ client/src/components/saveEvent.js
@@ -5,39 +5,31 @@ import "bootstrap/dist/css/bootstrap.css";
 function SaveEvent() {
   const location = useLocation();
   const navigate = useNavigate();
-  const tableData = location.state.tableData; // Retrieve passed table data
+  const tableData = location.state ? location.state.tableData : null; // Handle undefined state
 
-  // State for form fields
   const [eventName, setEventName] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [locationText, setLocationText] = useState('');
   const [additionalInfo, setAdditionalInfo] = useState('');
-
-  // Check if user is logged in
-  const userId = localStorage.getItem('userId');
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(!!userId);
-
-  // Get the user ID from local storage
-  //const userId = localStorage.getItem('userId'); 
-
-  // New state variable for form validation
   const [isFormValid, setIsFormValid] = useState(false);
 
-  // Validate form whenever relevant fields change
+  // Validate form and check user login status
   useEffect(() => {
+    const loggedInUserId = localStorage.getItem('userId');
+    const isUserLoggedIn = !!loggedInUserId;
     setIsFormValid(!!eventName.trim() && !!startDate && !!endDate && isUserLoggedIn);
-  }, [eventName, startDate, endDate, isUserLoggedIn]);
+  }, [eventName, startDate, endDate]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Check if mandatory fields are filled
-    if (!eventName.trim() || !startDate || !endDate) {
-      window.alert("Please fill out the Event Name, Start Date, and End Date.");
-      return; // Prevent the form from submitting
+    if (!isFormValid) {
+      window.alert("Please fill out the Event Name, Start Date, End Date, and ensure you are logged in.");
+      return;
     }
 
+    const userId = localStorage.getItem('userId'); // Get the user ID at submission time
     const eventData = {
       eventName,
       startDate,
@@ -49,7 +41,6 @@ function SaveEvent() {
     };
 
     try {
-      // Send a POST request to your server endpoint
       const response = await fetch('http://localhost:5050/saveEvent', {
         method: 'POST',
         headers: {
@@ -61,10 +52,9 @@ function SaveEvent() {
       const data = await response.text();
 
       if (response.ok) {
-        // Redirect or display success message
         navigate('/path-to-redirect');
       } else {
-        window.alert(data); // Display error message
+        window.alert(data);
       }
     } catch (error) {
       window.alert("There was a problem with saving the event: " + error.message);
@@ -73,51 +63,14 @@ function SaveEvent() {
 
   return (
     <div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-
+      <br/><br/><br/><br/><br/>
       <h2>Save Event Details</h2>
-      {/* Form for event details */}
-      <input
-        type="text"
-        value={eventName}
-        onChange={(e) => setEventName(e.target.value)}
-        placeholder="Event Name"
-      />
-      <input
-        type="date"
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-        placeholder="Start Date"
-      />
-      <input
-        type="date"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-        placeholder="End Date"
-      />
-      <input
-        type="text"
-        value={locationText}
-        onChange={(e) => setLocationText(e.target.value)}
-        placeholder="Location"
-      />
-      <input
-        type="text"
-        value={additionalInfo}
-        onChange={(e) => setAdditionalInfo(e.target.value)}
-        placeholder="Additional Info"
-      />
-      <button 
-        className={`btn btn-submit ${!isFormValid ? 'disabled-button' : ''}`} 
-        onClick={handleSubmit} 
-        disabled={!isFormValid}
-      >
-        Save Event Info
-      </button>
+      <input type="text" value={eventName} onChange={(e) => setEventName(e.target.value)} placeholder="Event Name" />
+      <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} placeholder="Start Date" />
+      <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} placeholder="End Date" />
+      <input type="text" value={locationText} onChange={(e) => setLocationText(e.target.value)} placeholder="Location" />
+      <input type="text" value={additionalInfo} onChange={(e) => setAdditionalInfo(e.target.value)} placeholder="Additional Info" />
+      <button className={`btn btn-submit ${!isFormValid ? 'disabled-button' : ''}`} onClick={handleSubmit} disabled={!isFormValid}>Save Event Info</button>
     </div>
   );
 }
